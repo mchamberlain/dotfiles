3e4b3cef578695b17e90ca1c76ce3eb71fa29ca9
Morné Chamberlain <thefreeman.za@gmail.com>
2013-10-22 00:23:11 +0200

    Initial commit.

---
+++ .zshrc
@@ -0,0 +1,89 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="af-magic"
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how many often would you like to wait before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+# COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git rails ruby systemd python)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory autocd extendedglob notify
+unsetopt beep nomatch
+# Disable auto-correction
+unsetopt correct correct_all
+bindkey -e
+
+# Key bindings
+# create a zkbd compatible hash;
+# to add other keys to this hash, see: man 5 terminfo
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+# setup key accordingly
+[[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+[[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-history
+[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-history
+[[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+[[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        printf '%s' ${terminfo[smkx]}
+    }
+    function zle-line-finish () {
+        printf '%s' ${terminfo[rmkx]}
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
+
+export PATH=~/bin:$PATH
---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Copyright (c) 2013 Morné Chamberlain
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+function link() {
+    src="$1"
+    target="$2"
+    if [ -e "$target" ]; then
+        if [ ! -L "$target" ]; then
+            echo "WARNING: $target exists but is not a symlink."
+        fi
+    else
+        parent_dir=`dirname "$target"`
+        if [ ! -e "$parent_dir" ]; then
+            mkdir -p "$parent_dir"
+        fi
+        echo "Creating $target"
+        ln -s "$src" "$target"
+    fi
+}
+
+pushd `dirname "$BASH_SOURCE"` > /dev/null
+
+link "$PWD/.oh-my-zsh" "$HOME/.oh-my-zsh"
+link "$PWD/.zshrc" "$HOME/.zshrc"
+link "$PWD/.vimrc" "$HOME/.vimrc"
+link "$PWD/.gitconfig" "$HOME/.gitconfig"
+link "$PWD/.gitmessage" "$HOME/.gitmessage"
+link "$PWD/issue" "$HOME/issue"
+
+link "$PWD/bin/hs" "$HOME/bin/hs"
+
+mkdir -p "$HOME/.vim"
+link "$PWD/.vim/autoload" "$HOME/.vim/autoload"
+link "$PWD/.vim/bundle" "$HOME/.vim/bundle"
+link "$PWD/.vim/colors" "$HOME/.vim/colors"
+link "$PWD/.vim/ftplugin" "$HOME/.vim/ftplugin"
+
+mkdir -p "$HOME/.config/sublime-text-2/Packages/User"
+link "$PWD/.config/sublime-text-2/Packages/User/Preferences.sublime-settings" "$HOME/.config/sublime-text-2/Packages/User/Preferences.sublime-settings"
+link "$PWD/.config/sublime-text-2/Packages/User/Package Control.sublime-settings" "$HOME/.config/sublime-text-2/Packages/User/Package Control.sublime-settings"
+link "$PWD/.config/sublime-text-2/Packages/tomorrow-theme" "$HOME/.config/sublime-text-2/Packages/tomorrow-theme"
+link "$PWD/.config/sublime-text-2/Packages/Theme - Soda" "$HOME/.config/sublime-text-2/Packages/Theme - Soda"
+
+mkdir -p "$HOME/.config/sublime-text-3/Packages/User"
+link "$PWD/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+link "$PWD/.config/sublime-text-3/Packages/User/Package Control.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/Package Control.sublime-settings"
+link "$PWD/.config/sublime-text-3/Packages/tomorrow-theme" "$HOME/.config/sublime-text-3/Packages/tomorrow-theme"
+link "$PWD/.config/sublime-text-3/Packages/Theme - Soda" "$HOME/.config/sublime-text-3/Packages/Theme - Soda"
+
+link "$PWD/.config/compton.conf" "$HOME/.config/compton.conf"
+
+link "$PWD/.config/openbox" "$HOME/.config/openbox"
+
+link "$PWD/.config/obmenu-generator" "$HOME/.config/obmenu-generator"
+
+popd > /dev/null
+
+unset -f link
